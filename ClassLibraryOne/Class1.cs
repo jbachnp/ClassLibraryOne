b37e3029e3ef22ae2b91fc0c0224d662d8d8823b
Jacques Bach <jbachnp@gmail.com>
2017-10-12 15:11:00 +0800

    yo

---
+++ ClassLibraryOne/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryOne
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        public string ReceiveMessage(string msg)
+        {
+            return string.Format("Message received: ", msg);
+
+        }
+    }
+}
